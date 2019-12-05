9596c0e2813a213b341fe7ccf21ab860a8a16a6a
Исаева Мария <isaeva-mariya@yandex.ru>
2019-12-05 14:34:24 +0300

    Updates

---
+++ precache-manifest.334ddc9bb1fdcdaae3c873ad1e820cda.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "173c9120d6ab3f30c7cc716b4c192652",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9045081db0203f69760",
+    "url": "/static/css/main.86023bef.chunk.css"
+  },
+  {
+    "revision": "a11a984dc436c9d4c86d",
+    "url": "/static/js/2.1212d665.chunk.js"
+  },
+  {
+    "revision": "f9045081db0203f69760",
+    "url": "/static/js/main.55c179ab.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  },
+  {
+    "revision": "21c3f28aa97bd787da041d65d33f8a7c",
+    "url": "/static/media/arrow.21c3f28a.svg"
+  },
+  {
+    "revision": "d246d0b54ed36b55eafe9759eefbb2c3",
+    "url": "/static/media/camera.d246d0b5.svg"
+  },
+  {
+    "revision": "22e2d4b14548bebc8710ea3b179bc697",
+    "url": "/static/media/chat_bubble.22e2d4b1.svg"
+  },
+  {
+    "revision": "09d840070c110fa893f4bd897f0f9cf9",
+    "url": "/static/media/checkmark.09d84007.svg"
+  },
+  {
+    "revision": "3dd7fca25bb76de88f332da8d62049ca",
+    "url": "/static/media/close.3dd7fca2.svg"
+  },
+  {
+    "revision": "dc4293ac2c1feda299b53e5b23c50b1e",
+    "url": "/static/media/grow.dc4293ac.svg"
+  },
+  {
+    "revision": "8e8cb4d0866b04b43097a46c3bd4c65b",
+    "url": "/static/media/like.8e8cb4d0.svg"
+  },
+  {
+    "revision": "3998f16bded130e5a6dca87917196dd1",
+    "url": "/static/media/logo.3998f16b.svg"
+  },
+  {
+    "revision": "9a5e3f826566569c41c4cba49484c572",
+    "url": "/static/media/next.9a5e3f82.svg"
+  },
+  {
+    "revision": "89e2eb56f623b446f71330960836a668",
+    "url": "/static/media/plus.89e2eb56.svg"
+  },
+  {
+    "revision": "ce089e28f14486df783eb9a2b1f85ba0",
+    "url": "/static/media/search.ce089e28.svg"
+  },
+  {
+    "revision": "0b2c91badcc836f33eddf834370043cc",
+    "url": "/static/media/star.0b2c91ba.svg"
+  },
+  {
+    "revision": "614b2b53a18ef4872a6dafad560bbebb",
+    "url": "/static/media/undo.614b2b53.svg"
+  },
+  {
+    "revision": "b0ee53e731b0687d5a7fe71ab5c0fc37",
+    "url": "/static/media/user.b0ee53e7.svg"
+  }
+]);
